67d5c26341ddfdecfb2fd2211171b300ffb543be
dt-woods <davisler@gmail.com>
2025-04-30 16:50:00 -0400

    redaction

--- cmu/ap4/Load_Workspace.m
+++
@@ -1,7 +0,0 @@
-%% Load AP4 workspace
-% Pulls in emissions data, S-R matrices, population & mortality rate data,
-% dose-response information, and willingness-to-pay & economic data
-clear
-load 'AP4_Control_Workspace.mat'
-
-%% end of script.
